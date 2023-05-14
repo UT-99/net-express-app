09c847be59a32bd4aac2442b2bb935071cdc47be
UT-99 <utkarshtiwari922@gmail.com>
2023-05-14 23:31:02 +0530

    Login page added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import "../Styles/Login.css";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setUser({
+      ...user,
+      [name]: value,
+    }); 
+    console.log("User in changeHandler method ")
+    console.log(user)
+  };
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    console.log("User in loginhandler method ")
+    console.log(user)
+
+    axios
+      .post("http://localhost:8000/login", user)
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.log("Some error occured while loging data ");
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <div className="align">
+        <label>Email : </label>
+        <br />
+        <input
+          type="text"
+          name="email"
+          value={user.email}
+          onChange={changeHandler}
+        />
+        <br />
+        <br />
+        <label>Password :</label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={changeHandler}
+        />
+        <br />
+        <button type="button" onClick={loginHandler}>
+          Login
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Login;
